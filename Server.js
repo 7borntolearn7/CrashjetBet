367b9562819dfd143e958dfe0299e296fdc66518
7borntolearn7 <97829493+7borntolearn7@users.noreply.github.com>
2023-12-31 11:09:40 +0530

    Updated Server.js

--- Server.js
+++ Server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const CrashBetRoutes = require("./Routes/route");
 app.use("/api/v1", CrashBetRoutes);
 
-app.listen(3000, () => {
-  console.log("listening at port 3000");
+app.listen(3002, () => {
+  console.log("listening at port 3002");
 });
+
