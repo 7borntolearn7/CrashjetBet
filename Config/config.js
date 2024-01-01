caf78e9076268a984d4816d8b8f9986e5bc23f84
7borntolearn7 <97829493+7borntolearn7@users.noreply.github.com>
2024-01-01 15:26:21 +0530

    Updated config.js2

--- Config/config.js
+++ Config/config.js
@@ -1,7 +1,10 @@
-// dbConfig.js
-module.exports = {
+const dataBase = require("mysql2/promise");
+
+const sqlPromise = dataBase.createPool({
   host: "stage-db-1.cmhfjy9fvag5.ap-south-1.rds.amazonaws.com",
   user: "admin",
-  database: "oscardb",
   password: "YNLx5TMezvpXZmS",
-};
+  database: "oscardb",
+});
+
+module.exports = { sqlPromise };
