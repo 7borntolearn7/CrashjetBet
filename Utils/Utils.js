bea4087b3d0631bbb4804617dd22fcbabf990137
7borntolearn7 <97829493+7borntolearn7@users.noreply.github.com>
2024-01-01 15:29:07 +0530

    Updated Utils.js2 and used sqlpromise.query

--- Utils/Utils.js
+++ Utils/Utils.js
@@ -1,31 +1,22 @@
-const mysql = require("mysql2/promise");
-const dbConfig = require("../Config/config");
+const { sqlPromise } = require("../Config/config");
 
 const fetchBalance = async (userId, token) => {
-  let connection = null;
-
   try {
-    // Create a MySQL connection
-    connection = await mysql.createConnection(dbConfig);
-
-    // Query the database to fetch the balance
-
-    const [result] = await connection.execute(
-      "SELECT LimitCurr FROM clientInfo WHERE id = ? AND token=?",
+    const result = await sqlPromise.query(
+      "SELECT LimitCurr FROM clientInfo WHERE id = ? AND token = ?",
       [userId, token]
     );
 
-    // Return balance or null if user not found
-    const balance = result.length > 0 ? result[0].LimitCurr : null;
+    console.log(result);
+
+    console.log("This query executed successfully");
+
+    const balance = result.length > 0 ? result[0][0].LimitCurr : null;
+    console.log(balance);
     return balance;
   } catch (error) {
     console.error("Error fetching balance:", error);
     throw error;
-  } finally {
-    // Release the connection when done
-    if (connection) {
-      await connection.end();
-    }
   }
 };
 
